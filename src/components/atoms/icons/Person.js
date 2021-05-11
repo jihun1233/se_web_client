1c8ceaa0bdecb075b8e2ce39582c2f93f290974e
jihun1233 <leejihun6535@gmail.com>
2021-05-11 19:01:57 +0900

    [ADD] 아이콘 및 아이콘 컴포넌트 추가
    - 아이콘 png 파일 및 컴포넌트 파일 추가
    
    - statics/assets에 icon png파일 추가
    - atoms/icons에 아이콘 아톰 컴포넌트들 추가

---
+++ src/components/atoms/icons/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import person from '../../../statics/assets/person.png';
+
+const Person = () => {
+  return (
+    <div>
+      <img src={person} alt="person" />
+    </div>
+  );
+};
+
+export default Person;
---
+++ src/components/atoms/icons/Talk.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import talk from '../../../statics/assets/talk.png';
+
+const Talk = () => {
+  return (
+    <div>
+      <img src={talk} alt="talk" />
+    </div>
+  );
+};
+
+export default Talk;
