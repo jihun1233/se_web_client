1e4767f5ec35f3f9a5e3d9f42db3e566d94ade13
jihun1233 <leejihun6535@gmail.com>
2021-05-29 23:15:12 +0900

    [UPDATE] Td, Th 컴포넌트의 props에 align 추가
    - text-align 요소에 들어갈 값을 선택해서 넣을 수 있도록 함.

--- src/components/atoms/Table/Td.js
+++ src/components/atoms/Table/Td.js
@@ -5,25 +5,28 @@ import styled from 'styled-components';
 const TdCustom = styled.td`
   overflow: hidden;
   white-space: nowrap;
+  ${props => (props.align ? `text-align: ${props.align}` : '')};
   &:overflow::after {
     content: 'a';
   }
 `;
-const Td = ({ children, colSpan, rowSpan }) => {
+const Td = ({ children, colSpan, rowSpan, align }) => {
   return (
-    <TdCustom colSpan={colSpan} rowSpan={rowSpan}>
+    <TdCustom colSpan={colSpan} rowSpan={rowSpan} align={align}>
       {children}
     </TdCustom>
   );
 };
 Td.defaultProps = {
   colSpan: 1,
-  rowSpan: 1
+  rowSpan: 1,
+  align: null
 };
 Td.propTypes = {
   children: PropTypes.arrayOf([]).isRequired,
   colSpan: PropTypes.number,
-  rowSpan: PropTypes.number
+  rowSpan: PropTypes.number,
+  align: PropTypes.string
 };
 
 export default Td;
