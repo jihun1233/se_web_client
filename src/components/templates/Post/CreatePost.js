34b1a85d5a77198e8e02dfa7c67f3ae80e95cc43
jihun1233 <leejihun6535@gmail.com>
2021-05-29 23:57:12 +0900

    [UPDATE, FIX] CreatePost 컴포넌트 수정
    - tag 기능 추가
    
    - AddTag 페이지를 popover를 통해 열어서 AddTag를 props로 전달하여 사용
    - 태그 기능을 추가하며 state에 리액트 컴포넌트를 저장하는것이 오류를 일으킨다는것을 알게됨. state와 setState를 통해 태그가 추가되면 반영되도록 구현해봤지만 state상에 컴포넌트로 전달된 tagContainer에는 최신화가 되지않음
    - 이를 해결하기위해 Table 형태로 되어있던 것을 각 컴포넌트를 테이블 없이 직접 배치하는것으로 변경

--- src/components/templates/Post/CreatePost.js
+++ src/components/templates/Post/CreatePost.js
@@ -3,10 +3,15 @@ import React, { useState, useEffect } from 'react';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import TableWithSpan from '../../modules/Table/TableWithSpan';
+import Popover from '@material-ui/core/Popover';
+// import TableWithSpan from '../../modules/Table/TableWithSpan';
+import { useHistory } from 'react-router-dom';
 import { createPost } from '../../../modules/post';
 import Editor from '../../modules/CKEditor/Editor';
 import TextField from '../../atoms/TextField/TextField';
+import Button from '../../atoms/Button/Button';
+import AddTag from '../Tag/AddTag';
+import TagContainer from '../../modules/TagContainer/TagContainer';
 
 const BoardTitle = styled.h1`
   text-align: center;
@@ -21,12 +26,45 @@ const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
 `;
+const GrowableContent = styled.div``;
 const ContentText = styled.div``;
+const ContentTitle = styled.div`
+  display: flex;
+  margin: 0.5rem;
+  & > * {
+    margin: 0.5rem;
+  }
+  align-items: center;
+  ${GrowableContent} {
+    flex-grow: 1;
+    self-justify: flex-start;
+  }
+`;
+const H2 = styled.h2``;
 const CreatePost = ({ match }) => {
+  const history = useHistory();
   const dispatch = useDispatch();
 
   const [title, setTitle] = useState();
   const [content, setContent] = useState();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [tagList, setTagList] = useState([]);
+  const addTag = ({ tagId, text }) => {
+    const currentIndex = tagList.findIndex(
+      currentTag => currentTag.tagId === tagId
+    );
+    if (currentIndex !== -1) return;
+    setTagList([...tagList, { tagId, tag: text }]);
+  };
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
 
   const titleOnChange = e => {
     setTitle(e.target.value);
@@ -41,59 +79,56 @@ const CreatePost = ({ match }) => {
         postContent: { text: content, title },
         isNotice: 'NORMAL',
         isSecret: 'NORMAL',
-        tagList: []
+        tagList: tagList.map(tag => ({
+          tagId: tag.tagId
+        }))
       })
     );
+
+    history.push(`/board/${match.params.boardId}`);
   };
 
-  const colgroup = [10, 10, 10, 10, 10, 10, 40];
-  const theads = [
-    {
-      data: <h1 style={{ fontSize: `1.5rem`, fontWeight: 'normal' }}>제목</h1>,
-      colSpan: 1
-    },
-    {
-      data: (
-        <TextField
-          value={title}
-          onChange={titleOnChange}
-          grow
-          fontSize="1.5rem"
-        />
-      ),
-      colSpan: 6
-    }
-  ];
-  const [tbodies, setTbodies] = useState([]);
-  const arrangePost = () => {
-    setTbodies([
-      [
-        { isTh: true, data: <p>태그</p> },
-        {
-          data: ''
-        },
-        { data: '', colSpan: 5 }
-      ]
-    ]);
-  };
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  useEffect(() => {
-    arrangePost();
-  }, []);
+
   return (
     <Container>
       <BoardTitle>{`게시판: ${match.params.boardId}`}</BoardTitle>
-      <TableWithSpan colgroup={colgroup} theads={theads} tbodies={tbodies} />
+      <ContentTitle>
+        <H2>제목</H2>
+        <GrowableContent>
+          <TextField value={title} onChange={titleOnChange} autoFocus grow />
+        </GrowableContent>
+      </ContentTitle>
+      <ContentTitle>
+        <H2>태그</H2>
+        <GrowableContent>
+          <TagContainer tagData={tagList} />
+        </GrowableContent>
+        <Button onClick={handleClick}>태그추가</Button>
+      </ContentTitle>
       <ContentText>
         <Editor value={content} onChange={contentOnChange} />
       </ContentText>
       <ButtonContainer>
-        <button type="button" onClick={createPostDispatch}>
-          완료
-        </button>
+        <Button onClick={createPostDispatch}>완료</Button>
       </ButtonContainer>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left'
+        }}
+      >
+        <AddTag key={tagList} addTag={addTag} />
+      </Popover>
     </Container>
   );
 };
