2e169f8a577f76f1bdd42548c8a33a4f245ccf4b
jihun1233 <leejihun6535@gmail.com>
2021-05-30 23:42:14 +0900

    [ADD] TagDeletable 컴포넌트 추가
    - 게시글 생성 시 태그를 추가했다가 다시 삭제할 수 있도록 삭제가능한 태그 컴포넌트 추가했다가
    
    - span으로 되어있는 태그 내부에 hover 시 x마크가 담겨있는 버튼이 추가되도록 함
    - 기존 태그도 배경과 글자색 일부 css 수정. button -> span으로 수정

--- src/components/atoms/Tag/Tag.js
+++ src/components/atoms/Tag/Tag.js
@@ -2,10 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const TagButton = styled.button`
-  background-color: white;
-  border-radius: 5px;
+const TagButton = styled.span`
+  background-color: #ffffff;
+  background-color: rgba(255, 255, 255, 0);
+  border-radius: 10px;
   border: 1px solid;
+  padding: 0.1rem 0.5rem;
+  border-color: ${props => props.theme.colors.mainColor};
+  color: ${props => props.theme.colors.mainColor};
 `;
 const Tag = ({ children }) => {
   return (
