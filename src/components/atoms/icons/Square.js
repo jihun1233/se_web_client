18632373980a55dec87869e16684b7e84235c4c5
jihun1233 <leejihun6535@gmail.com>
2021-05-31 17:39:44 +0900

    [ADD] 분홍색 사각형 div 컴포넌트 추가
    - 크기는 width, height 모두 1rem

---
+++ src/components/atoms/icons/Square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SquareDiv = styled.div`
+  width: 1rem;
+  height: 1rem;
+  background-color: #ffc0cb;
+`;
+const Square = () => {
+  return <SquareDiv />;
+};
+
+export default Square;
